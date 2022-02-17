9732be51cc1e0551a5e70263395f661ad9c696f8
Thạch Huỳnh <thach.huynh@techvsi.com>
2022-02-17 19:38:17 +0700

    feat:Thach-implement-cate-guider

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Categories;
+use App\Http\Controllers\tourGuideController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Admin.home');
 });
+
+
+// route of categories
+Route::get('/categories',[Categories::class, 'list']);
+Route::get('/categories/delete/{id}',[Categories::class, 'delete']);
+Route::get('/categories/edit/{id}',[Categories::class, 'form_edit']);
+Route::post('/categories/edit/{id}',[Categories::class, 'edit']);
+Route::get('/categories/add',[Categories::class, 'add_form']);
+Route::post('/categories/add',[Categories::class, 'add']);
+
+//route of guider
+Route::get('/guider',[tourGuideController::class, 'index']);
+Route::get('/guider/add', [tourGuideController::class, 'form_add']);
+Route::post('/guider/add', [tourGuideController::class, 'add']);
+Route::get('/guider/delete/{id}',[tourGuideController::class, 'delete']);
+Route::get('/guider/edit/{id}',[tourGuideController::class, 'form_edit']);
+Route::post('/guider/edit/{id}',[tourGuideController::class, 'edit']);
