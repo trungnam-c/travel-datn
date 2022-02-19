c94c7b90e30bc3a225764be50bcac70da74af394
quangdatcqd <[quangdatcqd@gmail.com]>
2022-02-19 11:23:02 +0700

    them location

--- resources/views/Admin/location_create.blade.php
+++ resources/views/Admin/location_create.blade.php
@@ -1,52 +1,65 @@
-@extends('admin.admin_layout')
+@extends('layouts.admin_layout')
 @section('location-active', 'active')
-@section('page-title','Quản lý địa điểm') 
+@section('page-title', 'Thêm địa điểm')
 @section('main')
     <section class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="card card-primary">
+
+                <div class="card card-primary col-sm-12">
                     <div class="card-header">
-                      <h3 class="card-title">Quick Example</h3>
+                        <h3 class="card-title">Thêm địa điểm mới</h3>
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
                     <form>
-                      <div class="card-body">
-                        <div class="form-group">
-                          <label for="exampleInputEmail1">Email address</label>
-                          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                        </div>
-                        <div class="form-group">
-                          <label for="exampleInputPassword1">Password</label>
-                          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                        </div>
-                        <div class="form-group">
-                          <label for="exampleInputFile">File input</label>
-                          <div class="input-group">
-                            <div class="custom-file">
-                              <input type="file" class="custom-file-input" id="exampleInputFile">
-                              <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-2"></div>
+                                <div class="col-sm-8">
+
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail1"
+                                            placeholder="Enter email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword1"
+                                            placeholder="Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputFile">File input</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" class="custom-file-input" id="exampleInputFile">
+                                                <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                            </div>
+                                            <div class="input-group-append">
+                                                <span class="input-group-text">Upload</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    </div>
+                                </div>
+                                {{-- col-6 --}}
+                                <div class="col-sm-2"></div>
                             </div>
-                            <div class="input-group-append">
-                              <span class="input-group-text">Upload</span>
-                            </div>
-                          </div>
                         </div>
-                        <div class="form-check">
-                          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        <!-- /.card-body -->
+
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
-                      </div>
-                      <!-- /.card-body -->
-      
-                      <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                      </div>
+
                     </form>
-                  </div>
+                </div>
+
             </div>
-            
+
             <!-- /.row -->
         </div>
         <!-- /.container-fluid -->
--- resources/views/Admin/location_index.blade.php
+++ resources/views/Admin/location_index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin_layout')
 @section('location-active', 'active')
-@section('page-title','Quản lý địa điểm') 
+@section('page-title', 'Quản lý địa điểm')
 @section('main')
     <section class="content">
         <div class="container-fluid">
@@ -11,7 +11,7 @@
 
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title"><a href="" class="btn btn-block btn-primary">Thêm địa điểm mới</a></h3>
+                            <h3 class="card-title"><a href="{{ route('location.create') }}" class="btn btn-block btn-primary">Thêm địa điểm mới</a></h3>
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
@@ -31,81 +31,84 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                   @php
-                                       $stt = 0;
-                                   @endphp
-                                    @foreach ($data as $row)
-
                                     @php
-                                    $stt ++;
-                                        $image = explode(",",$row->image);
-                                        $anhien = $row->anhien; 
+                                        $stt = 0;
                                     @endphp
+                                    @foreach ($data as $row)
+                                        @php
+                                            $stt++;
+                                            $image = explode(',', $row->image);
+                                            $anhien = $row->anhien;
+                                        @endphp
 
-                                    <tr class="location-tr">
-                                        <td>{{$stt}}</td>
-                                        <td>
-                                            <p>
-                                                Đi: <span class="data-span">{{$row->diemdi}}</span>
-                                            </p>
-                                            <p>
-                                                Đến: <span class="data-span">{{$row->diemden}} </span>
-                                            </p>
-                                        </td>
-                                        <td>
-                                            <p>Giá: <span class="data-span text-danger">{{number_format($row->giavetb)}}đ</span></p>
-                                            <p>Tgian: <span class="data-span">{{$row->time}} </span> </p>
-                                        </td>
-                                        <td>
-                                            <textarea  class="mota" id="" readonly cols="30" rows="4">{{$row->mota}}</textarea>
-                                        </td>
-                                        <td>
-                                            <p>PT: <span class="data-span">{{$row->phuongtien}}</span></p>
-                                            <p>Loại: <span class="data-span">Bãi biển</span></p>
-                                        </td>
-                                        <td>
-                                            <img src="{{$image[0]}}"
-                                                width="150px" alt="">
-                                        </td>
-                                        <td>
-                                            @if ($anhien)
-                                            <p><span class="text-success font-weight-bold">Đang Hiện</span></p>
-                                            @else
-                                            <p><span class="text-danger font-weight-bold">Đang Ẩn</span></p>
-                                            @endif
-                                            <p>Thứ tự: <span class="font-weight-bold">{{$row->top}}</span> </p>
-                                        </td>
-                                        <td>
-                                            <p class="edit-p">
-                                                <span class="edit-span" alt="Chỉnh sửa dòng này" ><i class="bi bi-pencil-square"></i></span> 
-                                                -- 
-                                                <span class="delete-span" alt="Xoá dòng này"><i class="bi bi-x-square"></i></span>    
-                                            </p>
-                                             
-                                        </td>
-                                    </tr>
+                                        <tr class="location-tr">
+                                            <td>{{ $stt }}</td>
+                                            <td>
+                                                <p>
+                                                    Đi: <span class="data-span">{{ $row->diemdi }}</span>
+                                                </p>
+                                                <p>
+                                                    Đến: <span class="data-span">{{ $row->diemden }} </span>
+                                                </p>
+                                            </td>
+                                            <td>
+                                                <p>Giá: <span
+                                                        class="data-span text-danger">{{ number_format($row->giavetb) }}đ</span>
+                                                </p>
+                                                <p>Tgian: <span class="data-span">{{ $row->time }} </span> </p>
+                                            </td>
+                                            <td>
+                                                <textarea class="mota" id="" readonly cols="30"
+                                                    rows="4">{{ $row->mota }}</textarea>
+                                            </td>
+                                            <td>
+                                                <p>PT: <span class="data-span">{{ $row->phuongtien }}</span></p>
+                                                <p>Loại: <span class="data-span">Bãi biển</span></p>
+                                            </td>
+                                            <td>
+                                                <img src="{{ $image[0] }}" width="150px" alt="">
+                                            </td>
+                                            <td>
+                                                @if ($anhien)
+                                                    <p><span class="text-success font-weight-bold">Đang Hiện</span></p>
+                                                @else
+                                                    <p><span class="text-danger font-weight-bold">Đang Ẩn</span></p>
+                                                @endif
+                                                <p>Thứ tự: <span class="font-weight-bold">{{ $row->top }}</span> </p>
+                                            </td>
+                                            <td>
+                                                <p class="edit-p">
+                                                    <span class="edit-span" alt="Chỉnh sửa dòng này"><i
+                                                            class="bi bi-pencil-square"></i></span>
+                                                    --
+                                                    <span class="delete-span" alt="Xoá dòng này"><i
+                                                            class="bi bi-x-square"></i></span>
+                                                </p>
+
+                                            </td>
+                                        </tr>
                                     @endforeach
-                                  
+
                                 </tbody>
                                 <tfoot>
-                                    
+
                                 </tfoot>
                             </table>
-                           
+
                         </div>
-                         
-                            <div class="div-paginate">
-                                {{$data->links('vendor.pagination.bootstrap-4')}}
-                            </div>
-                         
+
+                        <div class="div-paginate">
+                            {{ $data->links('vendor.pagination.bootstrap-4') }}
+                        </div>
+
                         <!-- /.card-body -->
                     </div>
                     <!-- /.card -->
                 </div>
                 <!-- /.col -->
-                
+
             </div>
-            
+
             <!-- /.row -->
         </div>
         <!-- /.container-fluid -->
