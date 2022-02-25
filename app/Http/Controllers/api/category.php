de05b2054d8d3256c9a3b9ae61672f58603c4bea
trungnam-c <namn71201@gmail.com>
2022-02-25 14:11:32 +0700

    controller api

---
+++ app/Http/Controllers/api/category.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\categoriesModel;
+
+use Illuminate\Http\Request;
+
+class category extends Controller
+{
+    public $md;
+    function __construct()
+    {
+        $this->md = new categoriesModel();
+    }
+    public function getall()
+    {
+        return $this->md::all();
+    }
+    public function getcateid($id)
+    {
+        return $this->md::find($id);
+    }
+    // SELECT id,ngaykhoihanh FROM detail_location WHERE idlocation = 1;
+}
