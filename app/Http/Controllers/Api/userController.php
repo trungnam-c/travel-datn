6333cf784187bf8c9188f535f3fc690087b51edf
Trung Nam <58066467+trungnam-c@users.noreply.github.com>
2022-02-28 15:33:54 +0700

    Update userController.php

--- app/Http/Controllers/Api/userController.php
+++ app/Http/Controllers/Api/userController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-
-use App\Models\Api\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
