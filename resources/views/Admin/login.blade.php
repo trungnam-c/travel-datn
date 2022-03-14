7348fab64664a55a8cd8c154b31888eaf1873e8d
Thạch Huỳnh <thach.huynh@techvsi.com>
2022-03-14 22:30:02 +0700

    cap nhat User interface cho login page

--- resources/views/Admin/login.blade.php
+++ resources/views/Admin/login.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,43 +38,39 @@
     <body>
         <div class="container">
             <div class="row">
-                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto mt-5">
                     <div class="card border-0 shadow rounded-3 my-5">
                         <div class="card-body p-4 p-sm-5">
-                            <h5 class="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
-                            <form>
+                            <h5 class="card-title text-center mb-5 fw-light fs-5">ĐĂNG NHẬP</h5>
+                            <form action="admin/login" method="POST">
+                                @csrf
                                 <div class="form-floating mb-3">
                                     <input type="email" class="form-control" id="floatingInput"
                                         placeholder="name@example.com">
-                                    <label for="floatingInput">Email address</label>
+                                    <label for="floatingInput">Địa chỉ email</label>
                                 </div>
                                 <div class="form-floating mb-3">
                                     <input type="password" class="form-control" id="floatingPassword"
                                         placeholder="Password">
-                                    <label for="floatingPassword">Password</label>
+                                    <label for="floatingPassword">Mật khẩu</label>
                                 </div>
 
                                 <div class="form-check mb-3">
                                     <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
                                     <label class="form-check-label" for="rememberPasswordCheck">
-                                        Remember password
+                                        Quên mật khẩu
                                     </label>
                                 </div>
-                                <div class="d-grid">
-                                    <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Sign
-                                        in</button>
-                                </div>
                                 <hr class="my-4">
-                                <div class="d-grid mb-2">
-                                    <button class="btn btn-google btn-login text-uppercase fw-bold" type="submit">
-                                        <i class="fab fa-google me-2"></i> Sign in with Google
-                                    </button>
-                                </div>
                                 <div class="d-grid">
-                                    <button class="btn btn-facebook btn-login text-uppercase fw-bold" type="submit">
-                                        <i class="fab fa-facebook-f me-2"></i> Sign in with Facebook
-                                    </button>
+                                    <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Đăng
+                                        nhập</button>
                                 </div>
+                                <div class="d-grid mt-2">
+                                    <button class="btn btn-success btn-login text-uppercase fw-bold" type="submit">Đăng
+                                        Ký</button>
+                                </div>
+
                             </form>
                         </div>
                     </div>
