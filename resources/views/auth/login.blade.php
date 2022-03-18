7ef27779e50d99cc4db54136ea6c11e969cfbdc4
Nam Nguyen <namn71201@gmail.com>
2022-03-18 22:07:53 +0700

    sua layout login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,7 +33,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+                    <div class="card-header">{{ __('Đăng nhập trang quản trị Viettravel') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
@@ -41,7 +41,7 @@
 
                             <div class="row mb-3">
                                 <label for="gmail"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Nhập email') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="gmail" type="email"
@@ -58,7 +58,7 @@
 
                             <div class="row mb-3">
                                 <label for="password"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Nhập mật khẩu') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
@@ -73,30 +73,13 @@
                                 </div>
                             </div>
 
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
                             <div class="row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
+                                        {{ __('Đăng nhập') }}
                                     </button>
 
-                                    @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                    @endif
+                    
                                 </div>
                             </div>
                         </form>
